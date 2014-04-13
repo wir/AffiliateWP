b24e0b5459fb669c62f5ef71f30d95bc0906d898
Pippin Williamson <pippin@pippinsplugins.com>
2014-04-12 21:50:59 -0500

    Start on integration. This will probably never get finished

--- includes/integrations/class-wishlist.php
+++ includes/integrations/class-wishlist.php
@@ -6,30 +6,41 @@ class Affiliate_WP_RCP extends Affiliate_WP_Base {
 
 		$this->context = 'rcp';
 
-		add_action( 'wishlistmember_registration_notification', array( $this, 'add_pending_referral' ), 10 );
+		add_action( 'init', array( $this, 'add_pending_referral' ), -1 );
 		add_action( 'rcp_insert_payment', array( $this, 'mark_referral_complete' ), 10, 3 );
 		//add_action( 'rcp_delete_payment', array( $this, 'revoke_referral_on_delete' ), 10 );
 
 		add_filter( 'affwp_referral_reference_column', array( $this, 'reference_link' ), 10, 2 );
 	}
 
-	public function add_pending_referral( $data ) {
+	public function add_pending_referral() {
 
-		echo '<pre>'; print_r( $data ); echo '</pre>';
-		echo '<pre>'; print_r( $_POST ); echo '</pre>'; exit;
+		if( ! function_exists( 'wlm_arrval' ) ) {
+			return;
+		}
 
+		if( is_admin() || empty( $_POST ) ) {
+			return;
+		}
+
+		echo '<pre>'; print_r( $_POST ); echo '</pre>';
+		//echo '<pre>'; print_r( $_POST ); echo '</pre>'; exit;
 
 		if( $this->was_referred() ) {
 
-			$user = get_userdata( $user_id );
+			if ( 'WPMRegister' == wlm_arrval( $_POST, 'WishListMemberAction' ) ) {
 
-			if( $this->get_affiliate_email() == $user->user_email ) {
-				return; // Customers cannot refer themselves
+				/*
+				if( $this->get_affiliate_email() == $user->user_email ) {
+					return; // Customers cannot refer themselves
+				}*/
+
+				$key = get_user_meta( $user_id, 'rcp_subscription_key', true );
+
+				$this->insert_pending_referral( 10, 'oaoisdadu9uasdas', 'Wishlist Test' );
+	
 			}
 
-			$key = get_user_meta( $user_id, 'rcp_subscription_key', true );
-
-			$this->insert_pending_referral( $price, $key, rcp_get_subscription( $user_id ) );
 		}
 
 	}
